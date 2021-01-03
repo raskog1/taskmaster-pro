b4fbbd732ccb87a6104a4454cc99d640bd03a4bd
Ryan Skog <ryan.skog@outlook.com>
2021-01-02 22:18:05 -0800

    Added jQuery UI interaction and Touch Punch for mobile

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,6 +159,59 @@ $("#remove-tasks").on("click", function () {
   saveTasks();
 });
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function (event) {
+    //console.log("activate", this);
+  },
+  deactivate: function (event) {
+    //console.log("deactivate", this);
+  },
+  over: function (event) {
+    //console.log("over", event.target);
+  },
+  out: function (event) {
+    //console.log("out", event.target);
+  },
+  update: function (event) {
+    const tempArr = [];
+
+    $(this).children().each(function () {
+      // Obtain the info from any lists that were altered
+      let text = $(this).find("p").text().trim();
+      let date = $(this).find("span").text().trim();
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    })
+    console.log(tempArr);
+
+    // Trim down list's ID to match object property
+    let arrName = $(this).attr("id").replace("list-", "");
+    // Update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function (event, ui) {
+    ui.draggable.remove();
+  },
+  over: function (event, ui) {
+    console.log("over");
+  },
+  out: function (event, ui) {
+    console.log("out");
+  }
+})
+
 // load tasks for the first time
 loadTasks();
 
